46ab99e2781c4ddca4cf723e7b92a3a108cd1982
Prabhav Basukala <161590929+PravabMarco@users.noreply.github.com>
2026-03-04 17:31:43 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vex-edge-media">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
